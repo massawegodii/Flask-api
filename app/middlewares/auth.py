73f1040549e93a4b672f90ffb40f3462c37a5471
massawegodii <massawegodfrey43@gmail.com>
2025-03-19 16:43:39 +0300

    protect the routes

--- app/middlewares/auth.py
+++ app/middlewares/auth.py
@@ -3,6 +3,7 @@ from flask_jwt_extended import verify_jwt_in_request, get_jwt_identity
 from functools import wraps
 from app.models.user_model import User
 
+# Verify Token
 def verify_token(role_required=None):
     def decorator(fn):
         @wraps(fn)
@@ -25,4 +26,30 @@ def verify_token(role_required=None):
                 return jsonify({"message": "Token is invalid or missing", "error": str(e)}), 401
         
         return wrapper
+    return decorator
+
+# Verify Roles and Permissions in routes
+def verify_roles(required_roles):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            try:
+                verify_jwt_in_request()
+
+                current_user_email = get_jwt_identity()
+                user = User.get_by_email(current_user_email)
+
+                if not user:
+                    return jsonify({"error": "User not found"}), 404
+
+                if user.role_name not in required_roles:
+                    return jsonify({"error": "Unauthorized. You do not have permissions"}), 403
+
+                return func(*args, **kwargs)
+
+            except Exception as e:
+                return jsonify({"error": str(e)}), 500
+        
+        wrapper.__name__ = func.__name__ 
+        return wrapper
+
     return decorator
--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from app.controllers.auth_controller import register, login, update_user_profile
-from app.middlewares.auth import verify_token
+from app.middlewares.auth import verify_roles, verify_token
 
 auth_bp = Blueprint("auth", __name__)
 user_bp = Blueprint("user", __name__)
@@ -12,6 +12,7 @@ auth_bp.route("/login", methods=["POST"])(login)
 
 # Update the user profile
 @user_bp.route("/update-profile", methods=["PUT"])
+@verify_roles(["ADMIN", "MANAGER"])
 @verify_token()
 def update_profile(user):
     return update_user_profile(user)
